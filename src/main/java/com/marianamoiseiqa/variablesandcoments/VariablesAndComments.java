7770d5a638cc343269dcabac190b98fb3452c109
MoiseiMarianaQA <moisei.maryana@gmail.com>
2023-05-09 18:26:37 +0300

    Added the Variables and Comments Task.

---
+++ src/main/java/com/marianamoiseiqa/variablesandcoments/VariablesAndComments.java
@@ -0,0 +1,41 @@
+package com.marianamoiseiqa.variablesandcoments;
+
+public class VariablesAndComments {
+    public static void main(String[] args) {
+        /* Declare the primitive data type variables*/
+        boolean falseBooleanVariable;
+        boolean trueBooleanVariable;
+        char firstCharFromAscii;
+        char lastCharFromAscii;
+        byte lowerLimitOfByteDataType;
+        byte upperLimitOfByteDataType;
+        short lowerLimitOfShortDataType;
+        short upperLimitOfShortDataType;
+        int lowerLimitOfIntDataType;
+        int upperLimitOFIntDataType;
+        long lowerLimitOfLongDataType;
+        long upperLimitOfLongDataType;
+        float lowerLimitOfFloatDataType;
+        float upperLimitOfFloatDataType;
+        double lowerLimitOfDoubleDataType;
+        double upperLimitOfDoubleDataType;
+        /*   Initialization of the above variables */
+        falseBooleanVariable = false;
+        trueBooleanVariable = true;
+        firstCharFromAscii = 'a';
+        lastCharFromAscii = 'Z';
+        lowerLimitOfByteDataType = -128;
+        upperLimitOfByteDataType = 127;
+        lowerLimitOfShortDataType = -32768;
+        upperLimitOfShortDataType = 32767;
+        lowerLimitOfIntDataType = -2147483648;
+        upperLimitOFIntDataType = 2147483647;
+        lowerLimitOfLongDataType = -9223372036854775808L;
+        upperLimitOfLongDataType = 9223372036854775807L;
+        lowerLimitOfFloatDataType = -3.4E+38f;
+        upperLimitOfFloatDataType = 3.4E+38f;
+        lowerLimitOfDoubleDataType = -1.7E+308d;
+        upperLimitOfDoubleDataType = 1.7E+305;
+        System.out.println(firstCharFromAscii);
+    }
+}
